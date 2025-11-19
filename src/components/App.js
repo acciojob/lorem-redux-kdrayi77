0acaa5fb3ec918d68b44dbda9f554930fbeb5e5e
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-11-19 22:14:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchLorem } from "./redux/loremSlice";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const { title, body, loading, error } = useSelector((state) => state.lorem);
 
-export default App
+  useEffect(() => {
+    dispatch(fetchLorem());
+  }, [dispatch]);
+
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h2>Lorem Redux Output</h2>
+
+      {loading && <p>Loading...</p>}
+
+      {error && (
+        <div style={{ color: "red" }}>
+          <p>Error: {error}</p>
+          <button onClick={() => dispatch(fetchLorem())}>Retry</button>
+        </div>
+      )}
+
+      {!loading && !error && (title || body) && (
+        <p data-testid="lorem-content">
+          {title && <strong>{title}</strong>}
+          {title && body && <br />}
+          {body}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default App;
