8217e74e14264c10be9f1b94961b8edf23299848
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-11-21 23:23:30 +0530

    Create reducer.js

---
+++ src/store/reducer.js
@@ -0,0 +1,27 @@
+import {
+  FETCH_LOREM_REQUEST,
+  FETCH_LOREM_SUCCESS,
+  FETCH_LOREM_FAILURE,
+} from "./actions";
+
+const initialState = {
+  loading: false,
+  data: null,
+  error: null,
+};
+
+export const loremReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_LOREM_REQUEST:
+      return { ...state, loading: true, error: null };
+
+    case FETCH_LOREM_SUCCESS:
+      return { ...state, loading: false, data: action.payload };
+
+    case FETCH_LOREM_FAILURE:
+      return { ...state, loading: false, error: action.error };
+
+    default:
+      return state;
+  }
+};
