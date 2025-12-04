41e6001b416865fa5bc12bf70f2d18e5b7bd2baf
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-12-04 21:43:59 +0530

    Delete src/redux directory

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import loremReducer from "./loremSlice";
-
-const store = configureStore({
-  reducer: { lorem: loremReducer }
-});
-
-export default store;
